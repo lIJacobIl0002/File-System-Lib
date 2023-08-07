faf061f2c9db7f7e7041199b682dae8ba322861f
Kenni <111925447+lIJacobIl0002@users.noreply.github.com>
2023-08-07 04:55:45 +0000

    Initial Commti

--- File-System-Lib.lua
+++ File-System-Lib.lua
@@ -1,12 +1,25 @@
 local http = game:GetService("HttpService");
 local Settings = { };
+
 Settings.__index = Settings;
+
+-- editing all files
+Settings.__gc = function(self)
+    for _, file in pairs(self.fileChanges) do
+        writefile(temp(self.folder, file), http:JSONEncode(self.files[file]))
+    end;
+end;
+
+function temp(folder, file)
+    return ('%s\\%s'):format(folder, file);
+end;
 
 function Settings.new(folder)
     local self = {};
 
     self.files = {};
     self.folder = folder;
+    self.fileChanges = {};
 
     if(not isfolder(folder)) then
         makefolder(folder)
@@ -17,10 +30,10 @@ function Settings.new(folder)
     return setmetatable(self, Settings);
 end;
 
+-- load files in folder (if folder does exist)
 function Settings:load_files()
 
     for i, v in pairs(listfiles(self.folder)) do
-        
         local file = string.match(v, "\\([%w%s%p]+)$");
         local _, fc = pcall(http.JSONDecode, http, readfile(v))
 
@@ -30,25 +43,41 @@ function Settings:load_files()
         if not _ then
             print(('failed to read %s due to \'%s\''):format(file, fc))
         end;
-
-    end
-
+    end;
 end;
 
+-- write to file
 function Settings:write_file(file, contents)
     assert(type(contents) == "table", 'table required')
+    table.insert(self.fileChanges, file);
 
-    self.files[file] = contents
-
-    local temp = ('%s\\%s'):format(self.folder, file)
-
-    writefile(temp, http:JSONEncode(contents))
+    self.files[file] = contents;
 end;
 
+-- read from file
 function Settings:read_file(file)
     assert(self.files[file], 'file does not exist')
 
     return self.files[file]
 end;
 
-return Settings
+-- delete file
+function Settings:delete_file(file)
+    if self.files[file] then
+        self.files[file] = nil;
+        
+        delfile(temp(self.folder, file));
+    end;
+end;
+
+-- if file eixsts
+function Settings:isfile(file)
+    return self.files[file] and true or false;
+end;
+
+--[[
+    * This makes modifications only to the table, but once garbage-collected it applies all changes.
+    * Since i didn't wanna apply changes to all files i kept track of file that are changed.
+]]
+
+return Settings;
